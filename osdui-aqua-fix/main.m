0bd4afcc6f5e3c270be880b8e8e0bafd706e6c1b
Denis Stanishevskiy <std.denis@gmail.com>
2020-09-09 18:36:31 +0300

    App project

---
+++ osdui-aqua-fix/main.m
@@ -0,0 +1,11 @@
+//
+//  osdui-aqua-fix
+//
+//  Created by Denis Stanishevskiy on 09.09.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
